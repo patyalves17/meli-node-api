55c30f15dc978b2cc7f16bc2b0ffee30a01addaa
Paty Alves <patriciaalves10@gmail.com>
2020-02-22 21:50:04 -0300

    mapper the results

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -1,9 +1,24 @@
-exports.items_all = function(req, res) {
-  items = [];
-  res.send(items);
+// import { itemsMapper } from './mappers/items.mapper.js';
+const itemsMapper = require('./mappers/items.mapper');
+const axios = require('axios');
+
+exports.items_all = async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`
+    );
+    const items = itemsMapper(response.data);
+    // res.send(response.data);
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 };
 
-exports.item_details = function(req, res) {
-  item = {};
+exports.item_details = async (req, res) => {
+  try {
+  } catch (error) {}
+
   res.send(item);
 };
---
+++ controllers/mappers/items.mapper.js
@@ -0,0 +1,44 @@
+const itemsMapper = data => {
+  return {
+    autor: {
+      name: 'Patricia',
+      lastname: 'Almeida'
+    },
+    categories: generateCategories(data),
+    items: generateItems(data)
+  };
+};
+
+const generateCategories = ({ filters }) => {
+  if (
+    filters &&
+    filters.length > 0 &&
+    filters[0].values &&
+    filters[0].values.length > 0
+  ) {
+    return filters[0].values[0]['path_from_root'].map(
+      category => category.name
+    );
+  } else {
+    return ['Outros'];
+  }
+};
+
+const generateItems = ({ results }) => {
+  return results.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      price: {
+        currency: item.currency_id,
+        amount: item.available_quantity,
+        decimals: item.price
+      },
+      picture: item.thumbnail,
+      condition: item.condition,
+      free_shipping: item.shipping.free_shipping
+    };
+  });
+};
+
+module.exports = itemsMapper;
